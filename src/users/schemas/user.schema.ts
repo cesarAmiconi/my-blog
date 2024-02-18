075b02a7b5e2e965dbd6cac801065b35ce6749c5
cesarAmiconi <cesar.amiconi@gmail.com>
2024-02-18 15:20:25 -0300

    Install Mongoose and create db

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: Boolean, default: false, required: true }) // Para probar como lo guarda en la base de datos
+  isAdmin: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
